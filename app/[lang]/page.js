71c1ce3239162f6c907703ba0c1558be607b362b
devhumayun <humayunkabir9408@gmail.com>
2024-09-20 23:32:14 +0800

    create a basic project layout

---
+++ app/[lang]/page.js
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+
+export default function Home() {
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div className="text-3xl font-bold text-red-600">Coming soo!</div>
+      </MaxWidthWrapper>
+    </>
+  );
+}
---
+++ components/max-width-wrapper.jsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils"
+
+const MaxWidthWrapper = ({ className, children }) => {
+    return (
+        <div
+            className={cn(
+                "w-full mx-auto max-w-[1400px] px-2.5 md:px-20 sm:px-10",
+                className
+            )}
+        >
+            {children}
+        </div>
+    )
+}
+
+export default MaxWidthWrapper
